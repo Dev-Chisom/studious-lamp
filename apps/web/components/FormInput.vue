0d079af48684f7af8bc11fecb815cb62343b6cbb
Chisom Onwugbenu <onwugbenuglory@gmail.com>
2025-05-10 01:10:41 +0100

    remove dark mode for web

--- apps/web/components/FormInput.vue
+++ apps/web/components/FormInput.vue
@@ -14,7 +14,7 @@
         v-if="icon" 
         class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
       >
-        <Icon :name="icon" class="h-5 w-5 text-gray-400 dark:text-gray-500 dark:text-gray-200" />
+        <Icon :name="icon" class="h-5 w-5 text-gray-400" />
       </div>
       
       <input
@@ -28,8 +28,7 @@
           'form-input',
           icon ? 'pl-10' : '',
           error ? 'border-error-300 focus:border-error-500 focus:ring-error-500' : '',
-          'bg-white dark:bg-gray-900 text-gray-900 border-gray-300 placeholder-gray-400',
-          'dark:bg-gray-900 dark:text-gray-100 dark:border-gray-700 dark:placeholder-gray-500'
+          'bg-white text-gray-900 border-gray-300 placeholder-gray-400',
         ]"
         @input="$emit('update:modelValue', $event.target.value)"
       />
@@ -40,7 +39,7 @@
       >
         <button
           type="button"
-          class="text-gray-400 hover:text-gray-500 dark:text-gray-200 dark:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 focus:outline-none"
+          class="text-gray-400 hover:text-gray-500 focus:outline-none"
           @click="togglePasswordVisibility"
         >
           <Icon
@@ -51,8 +50,8 @@
       </div>
     </div>
     
-    <p v-if="error" class="form-error text-error-600 dark:text-error-400">{{ error }}</p>
-    <p v-else-if="hint" class="mt-1 text-sm text-gray-500 dark:text-gray-200 dark:text-gray-400">{{ hint }}</p>
+    <p v-if="error" class="form-error text-error-600">{{ error }}</p>
+    <p v-else-if="hint" class="mt-1 text-sm text-gray-500">{{ hint }}</p>
   </div>
 </template>
 
